cf646112f8b87a5aba94e518c8e76e81edd40d0e
Parry Huang <parryhuang94@gmail.com>
2022-08-12 11:22:44 -0400

    commit tree 54071d1986ee38499c7f8daee3f9077bfce4cce4

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch(`https://anapioficeandfire.com/api/books`)
+  .then((request) => request.json())
+  .then((books) => bookCharacter(books))
 }
 
 function renderBooks(books) {
@@ -12,6 +14,31 @@ function renderBooks(books) {
   });
 }
 
+function bookFive(books) {
+  const main = document.querySelector('main');
+  const listing  = document.createElement('h2');
+  const name = books[4]['name'];
+  listing.innerHTML = name;
+  main.appendChild(listing);
+}
+function bookCharacter(books){
+  const main = document.querySelector('main');
+  
+  const h2 = document.createElement('h2');
+    h2.innerHTML = books[0]['characters'][0];
+    main.appendChild(h2);
+  }
+
+
+// function bookCharacter(books){
+//   const main = document.querySelector('main');
+//   books.forEach(book => {  
+//   const h2 = document.createElement('h2');
+//     h2.innerHTML = book['characters'][0];
+//     main.appendChild(h2);
+//   })
+// }
+
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
-});
+})
